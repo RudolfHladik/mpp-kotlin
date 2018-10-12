d713b9f1e5f2e88185a32f59592f8c8352f18388
rudolfhladik <hladik.rudolf@gmail.com>
2018-10-12 16:58:09 +0200

    init

---
+++ app/src/main/java/sample/Sample.kt
@@ -0,0 +1,18 @@
+package sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val h = hello()
+        val c = Sample().checkMe()
+
+        label.text = "$h my number is $c"
+    }
+}
